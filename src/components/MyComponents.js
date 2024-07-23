c439e4dd65cc0953b2e3e1be198f0652bdd2f104
Trangdz <trinhvantrangok123321@gmail.com>
2024-07-23 10:27:15 +0700

    them mycomponet , thay doi App

---
+++ src/components/MyComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponents extends React.Component{
+    //JSX
+    render(){
+        return(
+            <div>MY FIRST COMPONENT {Math.random()}</div>
+        );
+    }
+}
+export default MyComponents

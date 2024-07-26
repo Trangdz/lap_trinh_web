f9a2c731d527e59bfca9cb892a109951779f1224
Trangdz <trinhvantrangok123321@gmail.com>
2024-07-26 15:49:06 +0700

    way use react hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,44 +1,72 @@
-import React from "react";
+import React,{useState} from "react";
 
-class DisplayInfor extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        console.log(">>> call me constructor");
+// class DisplayInfor extends React.Component {
+//     constructor(props)
+//     {
+//         super(props);
+//         console.log(">>> call me constructor");
        
-    }
-    handleDelete=(id)=> {
-        this.props.handleDeleteUserToList(id);
+//     }
+//     handleDelete=(id)=> {
+//         this.props.handleDeleteUserToList(id);
         
-    }
+//     }
 
-    componentDidUpdate(prevProps, prevState,snapshot){
-        console.log(">>> call me DidUpdate",this.props,prevProps)
+//     componentDidUpdate(prevProps, prevState,snapshot){
+//         console.log(">>> call me DidUpdate",this.props,prevProps)
         
-    }
+//     }
 
-    componentDidMount(){
+//     componentDidMount(){
        
-        console.log('>>>call me componentDidMount');
-        setTimeout(() => {
-            document.title='Eric & Hoi Dan it'
-        }, 3000)
-    }
+//         console.log('>>>call me componentDidMount');
+//         setTimeout(() => {
+//             document.title='Eric & Hoi Dan it'
+//         }, 3000)
+//     }
 
 
 
-    render() {
-        console.log(">>>call me render");
-        const { listUser } = this.props;
+//     render() {
+//         console.log(">>>call me render");
+//         const { listUser } = this.props;
+//         return (
+//             <div>
+//                 {listUser.map((user) => {
+//                     return (
+//                         <div key={user.id}>
+//                             <div>My name is {user.name}</div>
+//                             <div>My address is {user.address}</div>
+//                             <div>My age is {user.age}</div>
+//                             <button onClick={()=>this.handleDelete(user.id)}>Delete</button>
+                            
+//                             <hr />
+//                         </div>
+//                     );
+//                 })}
+//             </div>
+//         );
+//     }
+// }
+// place onclick in button have use "()=>this.handleDelete(user.id)", else use "this.handleDelete(user.id)" it will execute immediately
+
+const DisplayInfor = (props) =>{
+        const {listUser}=props
+        const [isShowHideListUser,setShowHideListUser ]=useState(true);
+        const handleShowHide=()=>{
+            setShowHideListUser(!isShowHideListUser);
+        }
         return (
             <div>
+                <span onClick={()=>handleShowHide()}>{isShowHideListUser===true?"Hide information":"Show details"}</span>
                 {listUser.map((user) => {
                     return (
+                        isShowHideListUser &&
                         <div key={user.id}>
                             <div>My name is {user.name}</div>
                             <div>My address is {user.address}</div>
                             <div>My age is {user.age}</div>
-                            <button onClick={()=>this.handleDelete(user.id)}>Delete</button>
+                            <button onClick={()=>props.handleDeleteUserToList(user.id)}>Delete</button>
                             
                             <hr />
                         </div>
@@ -46,7 +74,7 @@ class DisplayInfor extends React.Component {
                 })}
             </div>
         );
-    }
+
 }
-// place onclick in button have use "()=>this.handleDelete(user.id)", else use "this.handleDelete(user.id)" it will execute immediately
+
 export default DisplayInfor;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -3,13 +3,17 @@ import UserInfor from "./Userinfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponents extends React.Component {
-    state = {
-        listUser: [
-            { id: "1", name: "Trang", address: "Thanh Hoa", age: 21 },
-            { id: "2", name: "Duy", address: "Ha Noi", age: 22 },
-            { id: "3", name: "Nhat", address: "Nam Dinh", age: 23 }
-        ]
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            listUser: [
+                { id: "1", name: "Trang", address: "Thanh Hoa", age: 21 },
+                { id: "2", name: "Duy", address: "Ha Noi", age: 22 },
+                { id: "3", name: "Nhat", address: "Nam Dinh", age: 23 }
+            ],
+            Hide: false
+        };
+    }
 
     handleAddUserToList = (newUser) => {
         const newListUser = [...this.state.listUser, { id: Date.now(), ...newUser }];
@@ -27,13 +31,13 @@ class MyComponents extends React.Component {
             listUser: newListUser
         });
     };
-
+    
     render() {
         return (
             <div>
                 <UserInfor onAddUser={this.handleAddUserToList} />
                 <br />
-                <DisplayInfor listUser={this.state.listUser} handleDeleteUserToList={this.handleDeleteUserToList} />
+                <DisplayInfor listUser={this.state.listUser} handleDeleteUserToList={this.handleDeleteUserToList}  />
             </div>
         );
     }
